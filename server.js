b267552656058ce729f44c7f4067eb4dff764c54
techesuraj <suraj.gantedi@gmail.com>
2017-04-05 09:56:53 -0400

    "Fixed localhost"

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req,res,next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-
+  if(req.headers['x-forwarded-proto'] === 'https'){
+    
+  res.redirect('http://' + req.hostname + req.url);
   }else{
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 });
 app.use(express.static('public'));
