b4a99b6f6aae1e80d3736801e5f10223e8895e2c
techesuraj <suraj.gantedi@gmail.com>
2017-04-04 23:18:32 -0400

    Updated Repo for Heroku

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -12,7 +12,7 @@ var Weather = React.createClass({
 	handleSearch:function(location){
 		var that = this;
 	
-		this.setState({isLoading:true})
+		this.setState({isLoading:true});
 		openWeatherMap.getTemp(location).then(function(temp){
 				that.setState({
 				location:location,
