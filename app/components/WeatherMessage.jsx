cc00e707bb7a9132a7336686130b34f017f46215
techesuraj <suraj.gantedi@gmail.com>
2017-04-04 23:41:21 -0400

    Updated

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,16 +1,5 @@
 var React = require('react');
 
-
-// var WeatherMessage = React.createClass({
-	
-// 	render: function(){
-// 		var {temp,location} = this.props;
-// 		return(
-// 			<h2>Current Weather in {location} is {temp}</h2>
-// 			);
-// 	}
-
-// });
 var WeatherMessage = ({temp,location}) => {
 	
 	return ( 
