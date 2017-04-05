d16bd70081d54c475476270253b9dc7ec3e85a30
techesuraj <suraj.gantedi@gmail.com>
2017-04-04 23:38:47 -0400

    updated

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -14,7 +14,7 @@ var React = require('react');
 var WeatherMessage = ({temp,location}) => {
 	
 	return ( 
-		<h2>Current Weather in {location} is {temp}</h2>
+		<h2>Current Weather is {temp} in {location}</h2>
 	);
 }
 module.exports = WeatherMessage;
