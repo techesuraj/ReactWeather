c96faff9a859d6ebc72fb275d4554ef80d43fcb4
techesuraj <suraj.gantedi@gmail.com>
2017-04-04 23:35:33 -0400

    Updated

---
+++ app/components/PageComponent.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var PageComponent = React.createClass({
+  render:function(){
+    return(
+      <div>
+        <h1>Current Weather</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = PageComponent;
